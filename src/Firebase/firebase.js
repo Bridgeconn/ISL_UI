6fad8bfd1e48c5ab4aba58f40c7b23a04f96eaa7
ashish-garg-25 <asg.pairbytes@gmail.com>
2022-05-22 23:50:51 +0530

    props added

---
+++ src/Firebase/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDCRTnpBA5ID5Xg1nJXgxngQBuDamW1Y8U",
+  authDomain: "isl-alignment.firebaseapp.com",
+  projectId: "isl-alignment",
+  storageBucket: "isl-alignment.appspot.com",
+  messagingSenderId: "970351417431",
+  appId: "1:970351417431:web:18812cfe99482754792b12"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
